bd45b27015deec03e688c5f7cf9d16d0390e57f0
Pawel Bednarczyk <bedpaw97@gmail.com>
2021-09-09 19:05:13 +0200

    #488 Create date helpers in date.js

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,8 +1,16 @@
+import moment from 'moment';
+
+export const DATE_FORMATS = {
+  DDMMYYYY: 'DD.MM.YYYY',
+};
+export function getDateInFormat (date, format = DATE_FORMATS.DDMMYYYY) {
+  return moment(date).format(format);
+}
+
 export const modifyDateHours = (date, modifyValue) => {
   const modifiedDate = new Date(date);
   const modifiedDateHour = date.getHours() + modifyValue;
   return new Date(modifiedDate.setHours(modifiedDateHour));
-
 };
 
 export const getZeroPad = (n) => (parseInt(n, 10) >= 10 ? '' : '0') + n;
@@ -15,4 +23,25 @@ export const getFullDateAsString = (date) => `${date.getHours()}` + ':' + getMin
 
 export const getHoursAndMinutesAsString = (date) => {
   return `${date.getHours()}` + ':' + getMinutesAsString(date);
+};
+
+export const splitObjectsListByTime = (objectsList, startDateKey, endDateKey) => {
+  const before = [];
+  const actual = [];
+  const after = [];
+
+  objectsList.map(obj => {
+    const startDate = obj[startDateKey];
+    const endDate = obj[endDateKey];
+    const timeNow = moment.now();
+
+    if (moment(timeNow).isBetween(startDate, endDate)) {
+      actual.push(obj);
+    } else if (moment(timeNow).isAfter(endDate)) {
+      after.push(obj);
+    } else {
+      before.push(obj);
+    }
+  });
+  return [before, actual, after];
 };
