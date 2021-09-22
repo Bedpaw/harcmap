761b6186020f02f79bee9a6c9bcf46a073f9e037
Pawel Bednarczyk <bedpaw97@gmail.com>
2021-09-22 18:36:03 +0200

    #493 Add basic implementation for a-checkbox and a-radio

---
+++ src/components/atoms/checkbox.vue
@@ -0,0 +1,49 @@
+<template>
+  <label :for="id">
+    <input
+      class="a-checkbox"
+      type="checkbox"
+      :id="id"
+      :checked="shouldBeChecked"
+      :value="value"
+      :disabled="isDisabled"
+      @change="updateInput"
+    />
+    <slot></slot>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'a-checkbox',
+  model: {
+    prop: 'modelValue',
+    event: 'change',
+  },
+  props: {
+    value: {
+      type: String,
+    },
+    modelValue: {
+      default: '',
+    },
+    id: {
+      default: '',
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    shouldBeChecked () {
+      return this.modelValue === this.value;
+    },
+  },
+  methods: {
+    updateInput () {
+      this.$emit('change', this.value);
+    },
+  },
+};
+</script>
---
+++ src/components/atoms/radio.vue
@@ -0,0 +1,49 @@
+<template>
+  <label :for="id">
+    <input
+      class="a-radio"
+      type="radio"
+      :id="id"
+      :checked="shouldBeChecked"
+      :value="value"
+      :disabled="isDisabled"
+      @change="updateInput"
+    />
+    <slot></slot>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'a-radio',
+  model: {
+    prop: 'modelValue',
+    event: 'change',
+  },
+  props: {
+    value: {
+      type: String,
+    },
+    modelValue: {
+      default: '',
+    },
+    id: {
+      default: '',
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    shouldBeChecked () {
+      return this.modelValue === this.value;
+    },
+  },
+  methods: {
+    updateInput () {
+      this.$emit('change', this.value);
+    },
+  },
+};
+</script>
