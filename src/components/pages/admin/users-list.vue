b0684fe870e8d2acf64c03bea93695a990d71d2b
Pawel Bednarczyk <bedpaw97@gmail.com>
2021-09-22 18:36:58 +0200

    #493 Create users-list page

---
+++ src/components/pages/admin/users-list.vue
@@ -0,0 +1,61 @@
+<template>
+  <t-search
+    :search-assist="$t('form.assist.searchUser')"
+    :elements="users"
+    :searchKeys="['user', 'userTeam']"
+  >
+    <template v-slot:result-list="{ elements: users }">
+      <div class="f-flex-1 f-scroll-default f-mt--2" >
+        <div
+          class="m-grid f-search-user f-mr-2"
+          v-for="user of users"
+          :key="user.user"
+        >
+          <div class="f-py-1 f-line-24">{{ user.userTeam }}</div>
+          <div class="f-pl-1 f-py-1 f-text-subtext f-text-14 f-line-24">{{ user.user }}</div>
+          <a-button-icon @click="openDetails(user)">
+            <a-icon :name="ICONS.more_vert"/>
+          </a-button-icon>
+        </div>
+        <o-popup-user-details ref="popup" :user="user"/>
+      </div>
+    </template>
+  </t-search>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import TSearch from 'templates/search';
+import AButtonIcon from 'atoms/button/icon';
+import OPopupUserDetails from 'organisms/popup/user-details';
+
+export default {
+  name: 'p-users-list',
+  components: {
+    OPopupUserDetails,
+    TSearch,
+    AButtonIcon,
+  },
+  data: () => ({
+    user: {},
+  }),
+  computed: {
+    ...mapGetters('allUsers', ['users']),
+  },
+  mounted () {
+    this.$store.dispatch('allUsers/download')
+      .then(() => {
+        this.errorMessage = '';
+      })
+      .catch(e => {
+        this.errorMessage = e.message;
+      });
+  },
+  methods: {
+    openDetails (user) {
+      this.user = user;
+      this.$refs.popup.toggle();
+    },
+  },
+};
+</script>
