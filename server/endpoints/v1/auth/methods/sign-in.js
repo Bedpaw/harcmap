cd526a28879e8e1af875e4270d88c377697ff279
Paweł Jurkiewicz <pawel.j@henouser.pl>
2021-10-18 19:53:18 +0200

    Refactor tests. Work on categories endpoint. Dump data for tests. Env for tests

--- server/endpoints/v1/auth/methods/sign-in.js
+++ server/endpoints/v1/auth/methods/sign-in.js
@@ -9,40 +9,40 @@ const getUserAggregation = require('../../../../aggregations/get-user');
 
 // Login
 async function signIn (request, response, next) {
-	const userIsAuthenticated = request.isAuthenticated();
-	const {
-		user,
-		body,
-	} = request;
-	const bodyLength = Object.keys(body).length;
+  const userIsAuthenticated = request.isAuthenticated();
+  const {
+    user,
+    body,
+  } = request;
+  const bodyLength = Object.keys(body).length;
 
-	if (!userIsAuthenticated || bodyLength) {
-		// run authenticate logic
-		passport.authenticate('local', (authenticateAppError, userData) => {
-			// authenticate errors
-			if (authenticateAppError || !userData) {
-				const errorCode = authenticateAppError
-					? errorCodes.INVALID_CREDENTIALS
-					: errorCodes.NOT_LOGGED;
+  if (!userIsAuthenticated || bodyLength) {
+    // run authenticate logic
+    passport.authenticate('local', (authenticateAppError, userData) => {
+      // authenticate errors
+      if (authenticateAppError || !userData) {
+        const errorCode = authenticateAppError
+          ? errorCodes.INVALID_CREDENTIALS
+          : errorCodes.NOT_LOGGED;
 
-				return handleErrors(new AppError(errorCode, {
-					httpStatus: 401,
-				}), request, response, next);
-			}
+        return handleErrors(new AppError(errorCode, {
+          httpStatus: 401,
+        }), request, response, next);
+      }
 
-			// run serialize user to session(request.user) logic
-			request.login(userData._id, (serializeAppError) => {
-				// serialization error
-				if (serializeAppError) {
-					return handleErrors(new AppError(errorCodes.SERIALIZE_ERROR, {
-						details: serializeAppError,
-					}), request, response, next);
-				}
+      // run serialize user to session(request.user) logic
+      request.login(userData._id, (serializeAppError) => {
+        // serialization error
+        if (serializeAppError) {
+          return handleErrors(new AppError(errorCodes.SERIALIZE_ERROR, {
+            details: serializeAppError,
+          }), request, response, next);
+        }
 
-				const responseData = {
-					email: userData.email,
-					userEvents: userData.userEvents,
-				};
+        const responseData = {
+          email: userData.email,
+          userEvents: userData.userEvents,
+        };
 
         response.send(responseData);
       });
--- server/models/events.js
+++ server/models/events.js
@@ -2,30 +2,30 @@ const Joi = require('joi');
 
 const Model = require('../libs/model');
 const {
-	eventName,
-	eventKey,
-	date,
-	defaultMapZoom,
-	mapCoordinates,
-	mapRefreshTime,
+  eventName,
+  eventKey,
+  date,
+  defaultMapZoom,
+  mapCoordinates,
+  mapRefreshTime,
 } = require('../libs/common-schemas');
 
 // event schema
 const eventSchema = {
-	eventName,
-	eventKey,
-	pointsCollectionName: Joi.string(),
-	categoriesCollectionName: Joi.string(),
-	eventDuration: {
-		startDate: date,
-		endDate: date,
-	},
-	mapProperties: {
-		zoom: defaultMapZoom,
-		longitude: mapCoordinates,
-		latitude: mapCoordinates,
-	},
-	eventRefreshTime: mapRefreshTime,
+  eventName,
+  eventKey,
+  pointsCollectionName: Joi.string(),
+  categoriesCollectionName: Joi.string(),
+  eventDuration: {
+    startDate: date,
+    endDate: date,
+  },
+  mapProperties: {
+    zoom: defaultMapZoom,
+    longitude: mapCoordinates,
+    latitude: mapCoordinates,
+  },
+  eventRefreshTime: mapRefreshTime,
 };
 
 // Create model
