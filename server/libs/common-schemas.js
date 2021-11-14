9280a396742df51f4853db2c26a79ba8a3a6124d
fzemi <filipzem11@gmail.com>
2021-11-14 10:18:31 -0500

    Create and narrow data schemes

--- server/libs/common-schemas.js
+++ server/libs/common-schemas.js
@@ -6,31 +6,52 @@ const Joi = require('joi');
 /**
  * Universal props
  */
-const date = Joi.number();
+const objectIdPattern = '^[a-fA-F0-9]{24,24}$'; // 24 character long hexadecimal pattern
+const keysPattern = '^[ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789]{4,4}$'; // except [O0Il] characters
+
+const date = Joi.number()
+  .integer()
+  .unsafe() // Date.now() max value is greater then js MAX_SAFE_INTEGER
+  .max(8640000000000000); // that's the max value of Date.now()
 const keys = Joi.string()
-  .length(4);
-const mapCoordinates = Joi.number();
-const objectIdInRequest = Joi.string();
+  .pattern(new RegExp(keysPattern));
+const mapCoordinates = Joi.object({
+  longitude: Joi.number()
+    .min(-180)
+    .max(180)
+    .required(),
+  latitude: Joi.number()
+    .min(-90)
+    .max(90)
+    .required(),
+});
+const objectIdInRequest = Joi.string()
+  .pattern(new RegExp(objectIdPattern));
 const objectIdInDatabase = Joi.object();
 
 /**
  * Users props
  */
-// TODO Password policy
+const passwordPattern = '^(?=.*[0-9])(?=[a-z]*)(?=.*[A-Z]).{8,24}$';
+
 const email = Joi.string()
   .email()
-  .max(24);
+  .max(24)
+  .trim();
 const password = Joi.string()
-  .min(8)
-  .max(24);
+  .pattern(new RegExp(passwordPattern));
 const role = Joi.string()
   .equal('creator', 'admin', 'teamLeader', 'teamMember');
-const teamName = Joi.string();
+const teamName = Joi.string()
+  .min(3)
+  .max(24)
+  .trim();
 
 /**
  * Teams props
  */
-const collectedPoints = Joi.array();
+const collectedPoints = Joi.array()
+  .items(objectIdInRequest);
 
 /**
  * Event props
@@ -38,21 +59,38 @@ const collectedPoints = Joi.array();
 const eventName = Joi.string()
   .min(3)
   .max(50);
-const eventRefreshTime = Joi.number();
-const defaultMapZoom = Joi.number();
+const eventRefreshTime = Joi.number()
+  .min(0)
+  .less(10000000000);
+const defaultMapZoom = Joi.number()
+  .min(0)
+  .less(10000000000);
 
 /**
  * Points props
  */
-const pointName = Joi.string();
-const pointType = Joi.string();
+const pointName = Joi.string()
+  .min(3)
+  .max(24)
+  .trim();
+const pointType = Joi.string()
+  .equal('timeout', 'permanent');
 
 /**
  * Category props
  */
-const pointValue = Joi.number();
-const pointShape = Joi.string();
-const categoryName = Joi.string();
+const pointValue = Joi.number()
+  .integer()
+  .min(0)
+  .max(9999999999);
+const pointShape = Joi.string()
+  .min(3)
+  .max(24)
+  .trim();
+const categoryName = Joi.string()
+  .min(3)
+  .max(24)
+  .trim();
 
 module.exports = {
   keys,
