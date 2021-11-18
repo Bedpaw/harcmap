063511d571f9d43abaa24afe1717b754d1e6f451
Pawel Bednarczyk <bedpaw97@gmail.com>
2021-11-18 18:59:48 +0100

    #536 Attach event config form to event-form template

--- client/src/components/pages/admin/edit-event.vue
+++ client/src/components/pages/admin/edit-event.vue
@@ -26,6 +26,7 @@ export default {
     this.defaultValues.mapLatitude = this.event.mapDefaultLatitude;
     this.defaultValues.mapLongitude = this.event.mapDefaultLongitude;
     this.defaultValues.mapZoom = this.event.mapDefaultZoom;
+    this.defaultValues.eventRules = this.event.eventRules;
   },
   computed: {
     ...mapGetters('event', [
