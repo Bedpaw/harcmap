fbff4ebb5ddbc681da345c72b94d7af09c725cc2
dbetka <kontakt@dbetka.pl>
2021-11-25 00:41:20 +0100

    #547 Improve optimization (splitting to chunks)

--- client/webpack/options/optimization.js
+++ client/webpack/options/optimization.js
@@ -2,8 +2,28 @@ const optimization = {
   splitChunks: {
     cacheGroups: {
       vendor: {
-        test: /[\\/]node_modules[\\/]/,
+        test: /node_modules\/(?!ol).*/,
         name: 'vendors',
+        chunks: 'all',
+      },
+      ol: {
+        test: /node_modules\/ol/,
+        name: 'ol',
+        chunks: 'all',
+      },
+      components: {
+        test: /src\/components\/(atoms|molecules|organisms|templates|mixins|extends)/,
+        name: 'components',
+        chunks: 'all',
+      },
+      pages: {
+        test: /src\/components\/pages/,
+        name: 'pages',
+        chunks: 'all',
+      },
+      utils: {
+        test: /src\/(utils|map)/,
+        name: 'utils',
         chunks: 'all',
       },
     },
