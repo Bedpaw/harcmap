0d48982f1198d2aaf8dcc267b08855dba0b3fba1
fzemi <filipzem11@gmail.com>
2021-11-29 14:31:51 -0500

    Remove unnecessary require

--- server/endpoints/v1/users/methods/update-user.js
+++ server/endpoints/v1/users/methods/update-user.js
@@ -1,5 +1,4 @@
 const Users = require('../../../../models/users');
-// const mongodb = require('../../../../libs/mongodb');
 const { ObjectId } = require('mongodb');
 
 async function updateUser (id, userObject) {
