473930fe55f96577ca365888f2ead449c6330823
Pawel Bednarczyk <bedpaw97@gmail.com>
2021-12-05 14:40:27 +0100

    #560 new api schema

---
+++ client/src/api/real/auth-controller.js
@@ -0,0 +1,60 @@
+import { API_ERRORS, API_WARNS } from 'utils/macros/errors';
+import { httpService } from 'config/http-service';
+
+const urls = {
+  signIn: '/auth/sign-in',
+  signUp: '/auth/sign-up',
+  signOut: '/auth/sign-out',
+};
+
+export const authController = {
+  signIn ({ user: email, password }) {
+    return httpService.post({
+      url: urls.signIn,
+      body: { email, password },
+      responseConfig: {
+        errorConfig: {
+          ...API_ERRORS.signIn,
+        },
+        warnConfig: {
+          ...API_WARNS.signIn,
+        },
+      },
+    });
+  },
+  checkYourLoginSession () {
+    return httpService.post({
+      url: urls.signIn,
+      responseConfig: {
+        errorConfig: {
+          ...API_ERRORS.checkYourLoginSession,
+        },
+      },
+    });
+  },
+  signUp ({ user: email, password }) {
+    return httpService.post({
+      url: urls.signUp,
+      body: {
+        email,
+        password,
+      },
+      responseConfig: {
+        errorConfig: {
+          ...API_ERRORS.signUp,
+
+        },
+      },
+    });
+  },
+  signOut () {
+    return httpService.delete({
+      url: urls.signOut,
+      responseConfig: {
+        errorConfig: {
+          ...API_ERRORS.signOut,
+        },
+      },
+    });
+  },
+};
