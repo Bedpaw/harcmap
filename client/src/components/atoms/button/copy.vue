8778e857dd057ba51deae50f423b5e1e89aa60da
dbetka <kontakt@dbetka.pl>
2022-01-28 19:22:29 +0100

    #566 Done copy button

--- client/src/components/atoms/button/copy.vue
+++ client/src/components/atoms/button/copy.vue
@@ -1,7 +1,8 @@
 <template>
   <a-button-secondary
     :disabled="disabled"
-    add-class="f-flex f-flex-just-center f-flex-al-center"
+    :add-class="'f-flex f-flex-just-center f-flex-al-center ' + doneClass"
+    @click="copy()"
   >
     <a-icon
       class="f-mr-1"
@@ -10,14 +11,15 @@
       outlined
     />
     <slot v-if="text === ''" />
-    {{ text }}
+    {{ showedMessage ? 'Copied!' : text }}
   </a-button-secondary>
 </template>
 
 <script lang="ts">
 import AButtonSecondary from 'atoms/button/secondary.vue';
+import { computed, defineComponent, ref } from 'vue';
 
-export default {
+export default defineComponent({
   name: 'a-button-copy',
   components: { AButtonSecondary },
   props: {
@@ -26,5 +28,33 @@ export default {
     disabled: { type: Boolean, default: false },
     text: { type: String, default: '' },
   },
-};
+  setup (props) {
+    let timeoutIdForMessage = setTimeout(() => undefined);
+    const showedMessage = ref(false);
+    const showedError = ref(false);
+    const doneClass = computed(() => showedMessage.value ? 'f-primary-bg' : '');
+
+    function copy () {
+      navigator.clipboard.writeText(props.textToCopy).then(
+        () => {
+          showedMessage.value = true;
+          clearTimeout(timeoutIdForMessage);
+          timeoutIdForMessage = setTimeout(() => {
+            showedMessage.value = false;
+          }, 1000);
+        },
+        () => {
+          showedError.value = true;
+        },
+      );
+    }
+
+    return {
+      copy,
+      showedMessage,
+      showedError,
+      doneClass,
+    };
+  },
+});
 </script>
