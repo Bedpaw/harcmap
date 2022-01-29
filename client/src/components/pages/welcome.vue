535eecb7b641d7ab60556d61b00478dc65dcd3ba
dbetka <kontakt@dbetka.pl>
2022-01-29 23:22:30 +0100

    #566 Rewrite welcome page to TS

--- client/src/components/pages/welcome.vue
+++ client/src/components/pages/welcome.vue
@@ -2,30 +2,41 @@
   <t-page title="">
     <div class="a-img f-map-round" />
     <div class="m-collection f-button">
-      <a-button-primary @click="$router.push(ROUTES.signIn.path)">
+      <a-button-primary @click="goTo(ROUTES.signIn.path)">
         {{ ROUTES.signIn.label }}
       </a-button-primary>
-      <a-button-secondary @click="$router.push(ROUTES.signUp.path)">
+      <a-button-secondary @click="goTo(ROUTES.signUp.path)">
         {{ ROUTES.signUp.label }}
       </a-button-secondary>
-      <a-button-secondary @click="$router.push(ROUTES.about.path)">
+      <a-button-secondary @click="goTo(ROUTES.about.path)">
         {{ ROUTES.about.label }}
       </a-button-secondary>
     </div>
   </t-page>
 </template>
 
-<script>
-import TPage from 'templates/page';
-import AButtonPrimary from 'atoms/button/primary';
-import AButtonSecondary from 'atoms/button/secondary';
+<script lang="ts">
+import TPage from 'templates/page.vue';
+import AButtonPrimary from 'atoms/button/primary.vue';
+import AButtonSecondary from 'atoms/button/secondary.vue';
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 
-export default {
+export default defineComponent({
   name: 'p-welcome',
   components: {
     AButtonSecondary,
     AButtonPrimary,
     TPage,
   },
-};
+  setup () {
+    const router = useRouter();
+    function goTo (path:string) {
+      return router.push(path);
+    }
+    return {
+      goTo,
+    };
+  },
+});
 </script>
