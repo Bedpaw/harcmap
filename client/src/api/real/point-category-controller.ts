e0aa74b5e841dc9f4744e7c650a881add0703964
Pawel Bednarczyk <bedpaw97@gmail.com>
2022-02-26 12:25:06 +0100

    #560 Handle edit and delete categories

--- client/src/api/real/point-category-controller.ts
+++ client/src/api/real/point-category-controller.ts
@@ -1,11 +1,13 @@
 import { httpService } from 'config/http-service';
 import { API_ERRORS } from 'src/utils/macros/errors';
-import { PointCategoryDTO, PointCategoryDTOCreate } from 'models/dtos/point';
+import { PointCategoryDTO, PointCategoryDTOCreate, PointCategoryDTOUpdate } from 'models/dtos/point';
 import { PointCategory } from 'models/point';
 
 const urls = {
   getCategoriesByEventId: (eventId: string) => '/events/' + eventId + '/categories',
   addPointCategory: (eventId: string) => '/events/' + eventId + '/categories',
+  updatePointCategory: (eventId: string, categoryId: string) => '/events/' + eventId + '/categories/' + categoryId,
+  deletePointCategory: (eventId: string, categoryId: string) => '/events/' + eventId + '/categories/' + categoryId,
 };
 
 export const pointCategoryController = {
@@ -20,6 +22,19 @@ export const pointCategoryController = {
     return httpService.post({
       url: urls.addPointCategory(eventId),
       body: category,
+      errorOptions: API_ERRORS.addPoint,
+    });
+  },
+  updatePointCategory (category: PointCategoryDTOUpdate, categoryId: string, eventId: string) {
+    return httpService.put({
+      url: urls.updatePointCategory(eventId, categoryId),
+      body: category,
+      errorOptions: API_ERRORS.addPoint,
+    });
+  },
+  deletePointCategory (categoryId: string, eventId: string) {
+    return httpService.delete({
+      url: urls.deletePointCategory(eventId, categoryId),
       errorOptions: API_ERRORS.addPoint,
     });
   },
