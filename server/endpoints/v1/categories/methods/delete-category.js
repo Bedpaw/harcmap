de98097184c40230c1a6aa9641dfbe4e5696a5aa
Paweł Jurkiewicz <pawel.j@henouser.pl>
2022-02-28 19:49:57 +0100

    #560 Fix id's of points categories. Check if category doesnt in use when removing

--- server/endpoints/v1/categories/methods/delete-category.js
+++ server/endpoints/v1/categories/methods/delete-category.js
@@ -1,9 +1,23 @@
 const Categories = require('../../../../models/categories');
+const Points = require('../../../../models/points');
 const { ObjectId } = require('mongodb');
+const {
+  AppError,
+  errorCodes,
+} = require('../../../../libs/errors');
 
 async function deleteCategory (eventIdString, categoryIdString) {
   const eventId = ObjectId(eventIdString);
   const categoryId = ObjectId(categoryIdString);
+
+  const categoriesInUse = await Points.get({ pointCategoryId: categoryId, eventId });
+
+  if (categoriesInUse) {
+    throw new AppError(errorCodes.CANNOT_DELETE_IN_USE_CATEGORY, {
+      httpStatus: 400,
+    });
+  }
+
   const { success } = await Categories.delete({ _id: categoryId, eventId });
 
   return { success };
