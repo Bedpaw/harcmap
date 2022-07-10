811b0734f1544ed6d1575eb54bd078ebfc332a9b
Paweł <bedpaw97@gmail.com>
2022-07-10 22:00:54 +0200

    #484 myPosition feature handling change position without destroy layer

--- client/src/map/factories/myPositionFeature.ts
+++ client/src/map/factories/myPositionFeature.ts
@@ -3,36 +3,46 @@ import Point from 'ol/geom/Point';
 import * as Projection from 'ol/proj';
 import { Fill, RegularShape, Stroke, Style } from 'ol/style';
 import { mapConfig } from 'map/config';
-import { colorsUtils } from 'utils/colors';
 import { GeoAccuracy } from 'utils/geolocation/geolocation-grade';
+import { map } from 'map';
+
+export const createMyPositionPointHelpers = {
+  getCoords: (longitude: number, latitude: number) => Projection.fromLonLat([longitude, latitude]),
+  getStyle: (accuracy: GeoAccuracy) => {
+    const { strokeColor, strokeWidth, getFillColor } = mapConfig.myPosition;
+
+    const stroke = new Stroke({
+      color: strokeColor,
+      width: strokeWidth,
+    });
+
+    const fill = new Fill({ color: getFillColor(accuracy) });
+
+    const pointValues = {
+      fill,
+      stroke,
+      ...mapConfig.points.shapes.point,
+    };
+
+    return new Style({
+      image: new RegularShape(pointValues),
+    });
+  },
+};
 
 export function createMyPositionFeature (myPosition: { latitude: number, longitude: number, accuracy: GeoAccuracy }) {
   const { latitude, longitude, accuracy } = myPosition;
-  const { strokeColor, strokeWidth, fillColor, getOpacity } = mapConfig.myPosition;
 
-  const stroke = new Stroke({
-    color: strokeColor,
-    width: strokeWidth,
-  });
-
-  const color = colorsUtils.hexOrRGBToRGB(fillColor, getOpacity(accuracy));
-  const fill = new Fill({ color });
-
-  const position = Projection.fromLonLat([longitude, latitude]);
+  const point = new Point(createMyPositionPointHelpers.getCoords(longitude, latitude));
 
   const feature = new Feature({
-    geometry: new Point(position),
+    geometry: point,
   });
 
-  const pointValues = {
-    fill,
-    stroke,
-    ...mapConfig.points.shapes.point,
-  };
+  feature.setStyle(createMyPositionPointHelpers.getStyle(accuracy));
 
-  feature.setStyle(
-    new Style({
-      image: new RegularShape(pointValues),
-    }));
+  map.myPosition.point = point;
+  map.myPosition.featurePoint = feature;
+
   return [feature];
 }
--- client/src/map/features/myPosition.ts
+++ client/src/map/features/myPosition.ts
@@ -1,18 +1,22 @@
 import { createFeatureCommon } from 'map/factories/featureCommon';
 import { Layer } from 'ol/layer';
 import { Source } from 'ol/source';
-import { createMyPositionFeature } from 'map/factories/myPositionFeature';
+import { createMyPositionFeature, createMyPositionPointHelpers } from 'map/factories/myPositionFeature';
 import { Map } from 'ol';
 import { map } from 'map';
 import { GeoAccuracy } from 'utils/geolocation/geolocation-grade';
 import { GeolocationPositionResult } from 'utils/geolocation/geolocation';
 import { mapConfig } from 'map/config';
+import Point from 'ol/geom/Point';
+import Feature from 'ol/Feature';
 
 interface MyPositionFeature {
   destroyAll: () => void,
   layer?: Layer<Source>,
   create: (config: { latitude: number, longitude: number, accuracy: GeoAccuracy }) => false | undefined;
   draw: (position: GeolocationPositionResult) => void;
+  point: null | Point
+  featurePoint: null | Feature<Point>
 }
 
 function draw (positionResult: GeolocationPositionResult) {
@@ -20,16 +24,8 @@ function draw (positionResult: GeolocationPositionResult) {
     return;
   }
   const {
-    details: {
-      lastAccuraciesGrade,
-      accuracy,
-    },
-    rawResult: {
-      coords: {
-        latitude,
-        longitude,
-      },
-    },
+    details: { lastAccuraciesGrade, accuracy },
+    rawResult: { coords: { latitude, longitude } },
   } = positionResult;
 
   switch (lastAccuraciesGrade) {
@@ -43,7 +39,6 @@ function draw (positionResult: GeolocationPositionResult) {
       });
       break;
     case GeoAccuracy.LOW:
-    case GeoAccuracy.VERY_LOW:
       map.myPosition.destroyAll();
       break;
   }
@@ -51,14 +46,28 @@ function draw (positionResult: GeolocationPositionResult) {
 
 export const myPosition: MyPositionFeature = {
   layer: undefined,
-  create: config => createFeatureCommon.createFeatureWrapper(
-    config,
-    createMyPositionFeature,
-    myPosition,
-  ),
+  create: (config) => {
+    if (map.myPosition.point && map.myPosition.featurePoint) {
+      const { longitude, latitude, accuracy } = config;
+
+      map.myPosition.point.setCoordinates(createMyPositionPointHelpers.getCoords(longitude, latitude));
+      map.myPosition.featurePoint.setStyle(createMyPositionPointHelpers.getStyle(accuracy));
+
+    } else {
+      return createFeatureCommon.createFeatureWrapper(
+        config,
+        createMyPositionFeature,
+        myPosition,
+      );
+    }
+  },
   draw,
+  point: null,
+  featurePoint: null,
   destroyAll () {
     if (!!map.realMap && !!myPosition.layer) {
+      myPosition.point = null;
+      myPosition.featurePoint = null;
       (map.realMap as unknown as Map).removeLayer(myPosition.layer);
     }
   },
