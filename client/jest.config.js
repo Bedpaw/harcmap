bb8479804088080510f885974ca2809071304e34
dbetka <kontakt@dbetka.pl>
2022-09-02 18:48:33 +0200

    #HAR-9 Fix moduleNameMapper configuration

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,8 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts-esm',
-  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 };
--- client/tests/utils/color.test.ts
+++ client/tests/utils/color.test.ts
@@ -1,4 +1,4 @@
-import { colorsUtils } from '../src/utils/colors';
+import { colorsUtils } from 'utils/colors';
 
 test('HEX to RGB', () => {
   expect(colorsUtils.hexToRGB('#FFFFFF')).toBe('rgb(255, 255, 255)');
