bde5ce64ca1318290af610266067eda67c2abfa5
dbetka <kontakt@dbetka.pl>
2022-09-20 15:28:45 +0200

    #HAR-16 #675 Use default validators and modifiers

--- ide-default/init-intellij-settings.js
+++ ide-default/init-intellij-settings.js
@@ -1,63 +1,30 @@
-const { initIntellijSettings, XMLUtils } = require('../../wdk/lib/ide/init-intellij-settings');
-
-const getByName = XMLUtils.getByName;
-const modifyValueByName = XMLUtils.modifyValueByName;
-const modifyAttr = XMLUtils.modifyAttr;
+const { initIntellijSettings } = require('@dbetka/wdk/lib/ide/init-intellij-settings');
+const { defaultModifiers } = require('@dbetka/wdk/lib/ide/default-modifiers');
+const { defaultValidators } = require('@dbetka/wdk/lib/ide/default-validators');
 
 initIntellijSettings([
   {
     name: 'EsLint',
     defaultXMLPath: '../wdk/share/ide/init-intellij-settings/default/eslint.xml',
     targetXMLPath: './.idea/inspectionProfiles/Project_Default.xml',
-    validator: json => {
-      if (json.component === undefined) return true;
-
-      const profile = json.component.profile[0];
-
-      return profile === undefined ||
-        profile.inspection_tool[0] === undefined;
-    },
-    modifier: json => {
-      const profile = json.component.profile[0];
-      const inspectionTool = profile.inspection_tool[0];
-
-      modifyAttr(inspectionTool, 'class', 'Eslint');
-      modifyAttr(inspectionTool, 'enabled', 'true');
-      modifyAttr(inspectionTool, 'level', 'WARNING');
-      modifyAttr(inspectionTool, 'enabled_by_default', 'true');
-
-      return json;
-    },
+    validator: defaultValidators.projectDefaultEslint(),
+    modifier: defaultModifiers.projectDefaultEslint(),
+    // replaceIfTargetInvalid: true,
   },
   {
     name: 'ESLint on save',
     defaultXMLPath: '../wdk/share/ide/init-intellij-settings/default/eslint-on-save.xml',
     targetXMLPath: './.idea/jsLinters/eslint.xml',
-    validator: json =>
-      json.project === undefined ||
-      json.project.component === undefined ||
-      Array.isArray(getByName(json.project.component, 'EslintConfiguration').option) === false,
-    modifier: json => {
-      const component = getByName(json.project.component, 'EslintConfiguration');
-      modifyValueByName(component.option, 'fix-on-save', 'true');
-
-      return json;
-    },
+    validator: defaultValidators.eslintOnSave(),
+    modifier: defaultModifiers.eslintOnSave({ enabled: 'true' }),
+    // replaceIfTargetInvalid: true,
   },
   {
     name: 'Webpack',
     defaultXMLPath: '../wdk/share/ide/init-intellij-settings/default/misc.xml',
     targetXMLPath: './.idea/misc.xml',
-    validator: json =>
-      json.project === undefined ||
-      json.project.component === undefined ||
-      Array.isArray(getByName(json.project.component, 'WebPackConfiguration').option) === false,
-    modifier: json => {
-      const component = getByName(json.project.component, 'WebPackConfiguration');
-      modifyValueByName(component.option, 'mode', 'MANUAL');
-      modifyValueByName(component.option, 'path', '$PROJECT_DIR$/client/webpack/config.common.js');
-
-      return json;
-    },
+    validator: defaultValidators.webpack(),
+    modifier: defaultModifiers.webpack({ dir: '$PROJECT_DIR$/client/webpack/config.common.js' }),
+    // replaceIfTargetInvalid: true,
   },
 ]);
